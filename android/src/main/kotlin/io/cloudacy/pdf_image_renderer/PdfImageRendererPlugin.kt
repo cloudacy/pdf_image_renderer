5c7487fc77d2a3f19e650dd7959d802452420ec0
Patrick <patrick.kuen11@gmail.com>
2020-04-25 01:08:02 +0200

    Remove packages string from package name because all folders with "packages" in it will not be uploaded to pub.dev https://dart.dev/tools/pub/publishing#what-files-are-published

--- android/src/main/kotlin/io/cloudacy/pdf_image_renderer/PdfImageRendererPlugin.kt
+++ android/src/main/kotlin/io/cloudacy/pdf_image_renderer/PdfImageRendererPlugin.kt
@@ -1,4 +1,4 @@
-package io.cloudacy.packages.pdf_image_renderer
+package io.cloudacy.pdf_image_renderer
 
 import android.graphics.Bitmap
 import android.graphics.Color
--- example/android/app/src/main/kotlin/io/cloudacy/packages/pdf_image_renderer_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/io/cloudacy/packages/pdf_image_renderer_example/MainActivity.kt
@@ -1,4 +1,4 @@
-package io.cloudacy.packages.pdf_image_renderer_example
+package io.cloudacy.pdf_image_renderer_example
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
